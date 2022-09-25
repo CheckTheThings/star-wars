71409eccaea57ddecf8c74e187a0966b8b219ca0
Dan Friedman <dan@thefriedmans.org>
2022-09-24 23:10:30 -0500

    Reorganize project/solution

--- src/CheckTheThings.StarWars.Parser/Program.cs
+++ src/CheckTheThings.StarWars.Parser/Program.cs
@@ -4,7 +4,7 @@ namespace CheckTheThings.StarWars
 {
     internal class Program
     {
-        public const string OutputDirectory = @"..\..\..\..\..\..\data";
+        public const string OutputDirectory = @"..\..\..\..\..\data";
         public const string ChecklistDirectory = $@"{OutputDirectory}\lists";
 
         private static async Task Main(string[] args)
